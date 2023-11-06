897b988f63201a8e378f6e737247f4b9b6a57fee
numbeeer31 <numbeeer31@gmail.com>
2023-11-07 01:03:30 +0330

    img mini

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
 })
 
 $(".alphaders .open").click(function () {
-    $(".alphaders .right").css("width", "75vw")
-    $(".alphaders .left").css("width", "25vw");
+    $(".alphaders .right").css("width", "80vw")
+    $(".alphaders .left").css("width", "20vw");
+    $(".alphaders .nav-item.sidbar").css("opacity", "0");
     $(".alphaders .add-animate").addClass("add");
     $(".alphaders .close").addClass("rotate").css("display", "flex");
     $(this).fadeOut(150);
@@ -21,17 +22,19 @@ $(".alphaders .close").click(function () {
     $(this).fadeOut(230);
     $(".alphaders .open").fadeIn();
     $(".alphaders .blur").removeClass("active");
+    $(".alphaders .nav-item.sidbar").css("opacity", "1");
 
 });
 // 
 $(".dynoapp .open").click(function () {
-    $(".dynoapp .right").css("width", "75vw")
-    $(".dynoapp .left").css("width", "25vw");
+    $(".dynoapp .right").css("width", "80vw")
+    $(".dynoapp .left").css("width", "20vw");
     $(".dynoapp .add-animate").addClass("add");
     $(".dynoapp .close").addClass("rotate").css("display", "flex");
     $(this).fadeOut(150);
     $(".dynoapp .close").fadeIn();
     $(".dynoapp .blur").addClass("active");
+    $(".dynoapp .nav-item.sidbar").css("opacity", "0");
 
 });
 $(".dynoapp .close").click(function () {
@@ -42,20 +45,23 @@ $(".dynoapp .close").click(function () {
     $(this).fadeOut(230);
     $(".dynoapp .open").fadeIn();
     $(".dynoapp .blur").removeClass("active");
+    $(".dynoapp .nav-item.sidbar").css("opacity", "1");
 
 });
 // 
 $(".onlineshop .open").click(function () {
-    $(".onlineshop .right").css("width", "75vw")
-    $(".onlineshop .left").css("width", "25vw");
+    $(".onlineshop .right").css("width", "80vw")
+    $(".onlineshop .left").css("width", "20vw");
     $(".onlineshop .add-animate").addClass("add");
     $(".onlineshop .close").addClass("rotate").css("display", "flex");
     $(this).fadeOut(150);
     $(".onlineshop .close").fadeIn();
     $(".onlineshop .blur").addClass("active");
+    $(".onlineshop .nav-item.sidbar").css("opacity", "0");
 
 });
 $(".onlineshop .close").click(function () {
+    $(".onlineshop .nav-item.sidbar").css("opacity", "1");
 
     $(".onlineshop .right").css("width", "65vw");
     $(".onlineshop .left").css("width", "35vw");
